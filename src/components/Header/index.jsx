df4e6e07cc7f4c5a18a41af3000d07543f640a4f
gabslr <gaby.lopez.rojas@gmail.com>
2024-06-25 22:15:17 -0600

    actualiza Header logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,10 +5,14 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <div className="logo">ALURAFLIX</div>
+      <div className="logo">
+        <Link to="/">
+          <img src="/imagenes/Logo.svg" alt="ALURAFLIX Logo" className="logo-image" />
+        </Link>
+      </div>
       <nav>
-        <Link to="/"><button>HOME</button></Link>
-        <Link to="/nuevo-video"><button>NUEVO VIDEO</button></Link>
+        <Link to="/"><button>Home</button></Link>
+        <Link to="/nuevo-video"><button>Nuevo Video</button></Link>
       </nav>
     </header>
   );
